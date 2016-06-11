0b0bb0a8b7d9288f2c6259b3181dce6924e1b9e3
root <putintsevnikita@gmail.com>
2016-06-11 21:20:55 +0600

    add std::map value iterator

--- natclass/include/natclass/problem.h
+++ natclass/include/natclass/problem.h
@@ -4,22 +4,25 @@
 #include <map>
 #include <vector>
 
+#include "utils.h"
+
 class Problem {
 public:
+    // Construction
     Problem (std::vector<std::map<std::string, int> value_mappings);
     Problem (std::string json);
 
+    // Serrialization
     std::string toJSON() const;
 
     int getFeatureCount() const;
-
     int encode(size_t feature_id, std::string value) const;
     std::string decode(size_t feature_id, std::string code) const;
-
     bool containsValue(size_t feature_id, std::string value) const;
 
-    // iterators through codes ?
-    // iterators through predicates ?
+    typedef MapValueIterator<std::map<int, std::string>::const_iterator> code_iterator;
+    code_value_iterator getBeginIter(size_t feature_id) const;
+    code_value_iterator getEndIter(size_t feature_id) const;
 
 private:
     std::vector<std::map<std::string, int>> value_mappings;
