0f75591c3034a99d0da5079c867aead9bfb33013
putintsevnikita@gmail.com <putintsevnikita@gmail.com>
2016-06-11 21:39:30 +0600

    add docstring to problem

--- natclass/include/natclass/problem.h
+++ natclass/include/natclass/problem.h
@@ -7,6 +7,11 @@
 #include "utils.h"
 
 class Problem {
+    /*
+     * Class represents metainformation for machine learning problem with categorical data
+     * It conatins features, values for each feature and their integer encodings.
+     */
+
 public:
     // Construction
     Problem (std::vector<std::map<std::string, int> value_mappings);
@@ -15,11 +20,13 @@ public:
     // Serrialization
     std::string toJSON() const;
 
+    // Checs and mappings
     int getFeatureCount() const;
     int encode(size_t feature_id, std::string value) const;
     std::string decode(size_t feature_id, std::string code) const;
     bool containsValue(size_t feature_id, std::string value) const;
 
+    // Iteration
     typedef MapValueIterator<std::map<int, std::string>::const_iterator> code_iterator;
     code_value_iterator getBeginIter(size_t feature_id) const;
     code_value_iterator getEndIter(size_t feature_id) const;
